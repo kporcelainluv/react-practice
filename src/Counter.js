54aebc50eba12e4e66be253c8892809cad543d93
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-02-25 16:06:29 +0300

    refactor Counter component, extract onClick func to separate func, add css

--- src/Counter.js
+++ src/Counter.js
@@ -2,25 +2,19 @@ import React, { useState } from "react";
 
 const Counter = () => {
   const [number, updateNumber] = useState(0);
+
+  const inc = () => updateNumber(number + 1);
+  const dec = () => updateNumber(number >= 0 ? number - 1 : 0);
+
   return (
     <div className="container">
       <h2 className="title">Add simple counter</h2>
-      <div style={{ margin: "auto" }}>
-        <button
-          className="counter-button"
-          onClick={() => {
-            return updateNumber(number + 1);
-          }}
-        >
+      <div className="counter-container">
+        <button className="button" onClick={inc}>
           +
         </button>
-        {number}
-        <button
-          className="counter-button"
-          onClick={() => {
-            return updateNumber(number >= 0 ? number - 1 : 0);
-          }}
-        >
+        <span className="counter-number">{number}</span>
+        <button className="button" onClick={dec}>
           -
         </button>
       </div>
