a6ae8d94c81388a0f722e2d55e39b11532d75885
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-02-25 17:37:37 +0300

    add useReducer

--- src/Counter2.js
+++ src/Counter2.js
@@ -1,32 +1,45 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
+import nanoid from "nanoid";
 
-const increase = (list, index) => {
-  return list.map((elm, i) => {
-    if (i === index) return { ...elm, value: elm.value + 1 };
-    else return elm;
-  });
+const reducer = (counters, action) => {
+  const type = action.type;
+  if (type === "increment") {
+    const { id } = action.payload;
+    return counters.map(elm => {
+      if (elm.id === id) return { ...elm, value: elm.value + 1 };
+      else return elm;
+    });
+  } else if (type === "decrement") {
+    const { id } = action.payload;
+    return counters.map(elm => {
+      if (elm.id === id)
+        return { ...elm, value: elm.value > 0 ? elm.value - 1 : 0 };
+      else return elm;
+    });
+  } else if (type === "deleteCounter") {
+    const { id } = action.payload;
+    return counters.filter(elm => elm.id !== id);
+  } else if (type === "addCounter") {
+    return [...counters, { id: nanoid(), value: 0 }];
+  }
+  return counters;
 };
-const decrease = (list, index) => {
-  return list.map((elm, i) => {
-    if (i === index) return { ...elm, value: elm.value - 1 };
-    else return elm;
-  });
-};
-const addCounter = list => {
-  return [...list, { id: Math.random(), value: 0 }];
-};
-const deleteCounter = (list, counterIndex) => {
-  return list.filter((elm, index) => index !== counterIndex);
-};
+
+const initialValue = [
+  {
+    id: nanoid(),
+    value: 0
+  }
+];
 
 export const AdvancedCounter = () => {
-  // TODO: use reducer
-  const [counters, updateCounters] = useState([
-    {
-      id: 0,
-      value: 0
-    }
-  ]);
+  const [counters, dispatch] = useReducer(reducer, initialValue);
+
+  const increment = id => dispatch({ type: "increment", payload: { id } });
+  const decrement = id => dispatch({ type: "decrement", payload: { id } });
+  const deleteCounter = id =>
+    dispatch({ type: "deleteCounter", payload: { id } });
+  const addCounter = () => dispatch({ type: "addCounter", payload: {} });
 
   return (
     <div className="container">
@@ -36,7 +49,7 @@ export const AdvancedCounter = () => {
         <li>add counter with 0 value by default</li>
         <li>remove specific counter</li>
       </ul>
-      <div style={{ margin: "50px auto" }}>
+      <div className="advanced-counter__container">
         <form
           onSubmit={e => {
             e.preventDefault();
@@ -52,18 +65,14 @@ export const AdvancedCounter = () => {
                   <button
                     className="button"
                     type="submit"
-                    onClick={() => {
-                      updateCounters(deleteCounter(counters, index));
-                    }}
+                    onClick={() => deleteCounter(value.id)}
                   >
                     Delete counter
                   </button>
                   <button
                     className="button"
                     value="-"
-                    onClick={() => {
-                      updateCounters(decrease(counters, index));
-                    }}
+                    onClick={() => decrement(value.id)}
                   >
                     -
                   </button>
@@ -73,9 +82,7 @@ export const AdvancedCounter = () => {
                   <button
                     className="button"
                     value="+"
-                    onClick={() => {
-                      updateCounters(increase(counters, index));
-                    }}
+                    onClick={() => increment(value.id)}
                   >
                     +
                   </button>
@@ -83,9 +90,7 @@ export const AdvancedCounter = () => {
                   <button
                     className="button"
                     type="button"
-                    onClick={() => {
-                      updateCounters(addCounter(counters, index));
-                    }}
+                    onClick={() => addCounter()}
                   >
                     Add counter
                   </button>
