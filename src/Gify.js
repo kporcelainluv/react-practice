306964c3817d52f8b85beff8e2854c467147622e
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-02-25 21:11:24 +0300

    refactor Gify

--- src/Gify.js
+++ src/Gify.js
@@ -1,76 +1,87 @@
 import React, { useEffect, useState } from "react";
-import nanoid from "nanoid";
 
-export const Gify = () => {
-  const [state, setState] = useState({
-    query: "scrubs",
-    gifs: [],
-    input: ""
-  });
-
-  useEffect(() => {
-    (() => {
-      const url = new URL("https://api.giphy.com/v1/gifs/search");
-
-      const params = {
-        api_key: "GRTDQB888zEjwAMOjmf1QeV8Fn51IulA",
-        q: state.query,
-        limit: 1,
-        contentType: "application/json",
-        dataType: "json"
-      };
-      Object.keys(params).forEach(key =>
-        url.searchParams.append(key, params[key])
-      );
-      fetch(url)
-        .then(response => response.json())
-        .then(response => {
-          const images = response.data;
-          setState(s => {
-            return { ...s, gifs: [...images, ...s.gifs] };
-          });
-        });
-    })();
-  }, [state.query]);
-
+const Form = ({ state, updateQuery, updateInput }) => {
   return (
-    <div className="container">
-      <form
-        style={{ margin: "auto" }}
-        onSubmit={e => {
-          e.preventDefault();
-        }}
-      >
-        <label>
-          <h2 className="title">Type in a word and get a gif</h2>
-          <input
-            className="input"
-            type="text"
-            value={state.input}
-            onChange={e => {
-              const currentInput = e.target.value;
-              setState(s => {
-                return { ...s, input: currentInput };
-              });
-            }}
-          />
-        </label>
+    <form
+      className="container--centered"
+      onSubmit={e => {
+        e.preventDefault();
+      }}
+    >
+      <h2 className="title">Type in a word and get a gif</h2>
+      <div className="input-container">
+        <input
+          className="input"
+          type="text"
+          placeholder={state.input}
+          onChange={e => {
+            updateInput(e.target.value);
+          }}
+        />
         <button
-          className="button"
+          className="black-button"
           onClick={() => {
-            setState(s => {
-              return { ...s, query: state.input };
-            });
+            updateQuery(state.input);
           }}
           type="submit"
         >
           Enter
         </button>
-      </form>
+      </div>
+    </form>
+  );
+};
 
+const getGifs = query => {
+  const url = new URL("https://api.giphy.com/v1/gifs/search");
+
+  const params = {
+    api_key: "GRTDQB888zEjwAMOjmf1QeV8Fn51IulA",
+    q: query,
+    limit: 1,
+    contentType: "application/json",
+    dataType: "json"
+  };
+  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+
+  return fetch(url)
+    .then(response => response.json())
+    .then(response => response.data);
+};
+
+export const Gify = () => {
+  const [state, setState] = useState({
+    query: "scrubs",
+    gifs: [],
+    input: "Scrubs"
+  });
+
+  const updateQuery = input => {
+    return setState(s => {
+      return { ...s, query: input };
+    });
+  };
+
+  const updateInput = input => {
+    return setState(s => {
+      return { ...s, input: input };
+    });
+  };
+
+  useEffect(() => {
+    getGifs(state.query).then(gif => {
+      setState(s => {
+        return { ...s, gifs: [...gif, ...s.gifs] };
+      });
+    });
+  }, [state.query]);
+
+  return (
+    <div className="container">
+      <Form state={state} updateInput={updateInput} updateQuery={updateQuery} />
       {state.gifs.map(obj => {
         return (
-          <div key={nanoid()} style={{ margin: "auto" }}>
+          <div key={obj.id} className="container--centered">
             <img
               src={obj.images.fixed_height.url}
               alt={obj.title}
