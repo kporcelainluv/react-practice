51811c81b5456ca904e0f3424ec644261dea5146
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-02-25 17:05:50 +0300

    refactor css, refactor funcs

--- src/Counter2.js
+++ src/Counter2.js
@@ -1,36 +1,37 @@
 import React, { useState } from "react";
 
-const increaseNum = (list, index) => {
-  return [
-    ...list.slice(0, index),
-    { ...list[index], value: list[index].value + 1 },
-    ...list.slice(index + 1)
-  ];
+const increase = (list, index) => {
+  return list.map((elm, i) => {
+    if (i === index) return { ...elm, value: elm.value + 1 };
+    else return elm;
+  });
 };
-const decreaseNum = (list, index) => {
-  return [
-    ...list.slice(0, index),
-    { ...list[index], value: list[index].value - 1 },
-    ...list.slice(index + 1)
-  ];
+const decrease = (list, index) => {
+  return list.map((elm, i) => {
+    if (i === index) return { ...elm, value: elm.value - 1 };
+    else return elm;
+  });
 };
 const addCounter = list => {
   return [...list, { id: Math.random(), value: 0 }];
 };
-const deleteCounter = (list, index) => {
-  return [...list.slice(0, index), ...list.slice(index + 1, list.length)];
+const deleteCounter = (list, counterIndex) => {
+  return list.filter((elm, index) => index !== counterIndex);
 };
+
 export const AdvancedCounter = () => {
+  // TODO: use reducer
   const [counters, updateCounters] = useState([
     {
       id: 0,
       value: 0
     }
   ]);
+
   return (
     <div className="container">
       <h2 className="title">Advanced counter</h2>
-      <ul style={{ margin: "auto", paddingLeft: "85px" }}>
+      <ul className="advanced-counter__list">
         <li>several counters at once</li>
         <li>add counter with 0 value by default</li>
         <li>remove specific counter</li>
@@ -43,45 +44,52 @@ export const AdvancedCounter = () => {
         >
           {counters.map((value, index) => {
             return (
-              <div key={value.id}>
-                <button
-                  className="counter-button"
-                  type="submit"
-                  onClick={() => {
-                    updateCounters(deleteCounter(counters, index));
-                  }}
-                >
-                  Delete counter
-                </button>
-                <button
-                  className="counter-button"
-                  value="-"
-                  onClick={() => {
-                    updateCounters(decreaseNum(counters, index));
-                  }}
-                >
-                  -
-                </button>
-                <span> {value.value}</span>
-                <button
-                  className="counter-button"
-                  value="+"
-                  onClick={() => {
-                    updateCounters(increaseNum(counters, index));
-                  }}
-                >
-                  +
-                </button>
+              <div
+                key={value.id}
+                className="advanced-counter__buttons-container"
+              >
+                <div>
+                  <button
+                    className="button"
+                    type="submit"
+                    onClick={() => {
+                      updateCounters(deleteCounter(counters, index));
+                    }}
+                  >
+                    Delete counter
+                  </button>
+                  <button
+                    className="button"
+                    value="-"
+                    onClick={() => {
+                      updateCounters(decrease(counters, index));
+                    }}
+                  >
+                    -
+                  </button>
+                </div>
+                <span className="counter-number"> {value.value}</span>
+                <div className="advanced-counter__buttons">
+                  <button
+                    className="button"
+                    value="+"
+                    onClick={() => {
+                      updateCounters(increase(counters, index));
+                    }}
+                  >
+                    +
+                  </button>
 
-                <button
-                  className="counter-button"
-                  type="button"
-                  onClick={() => {
-                    updateCounters(addCounter(counters, index));
-                  }}
-                >
-                  Add new counter
-                </button>
+                  <button
+                    className="button"
+                    type="button"
+                    onClick={() => {
+                      updateCounters(addCounter(counters, index));
+                    }}
+                  >
+                    Add counter
+                  </button>
+                </div>
               </div>
             );
           })}
