044d815a2c4f6b9b384998cab7eecabe8f20773f
Ksusha Zhukova <zhukovaksusha@gmail.com>
2020-02-26 19:26:02 +0300

    add Error component

---
+++ src/Error.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Error = () => {
+  return (
+    <div className="container" style={{ maxWidth: " 300px" }}>
+      Error. Try to reload the page
+    </div>
+  );
+};
